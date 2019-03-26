7536ec20726c92ef1ff9f861ca9c072a9c608dea
Daniele Perera <daniele.perera@cybaze.it>
2019-03-26 11:19:27 +0100

    added colors and tqdm progressbar

--- src/__main__.py
+++ src/__main__.py
@@ -5,6 +5,12 @@ import webscapper
 import requests
 import category_abuseipdb
 from tqdm import tqdm
+from colorama import Fore, init
+
+iconOK = (Fore.GREEN + '[!]')
+init(autoreset=True)
+
+#print(iconOK)
 
 # ------- Get info about attacker, victim, context from the webscapper -----
 # info = webscapper.get_info()
@@ -53,7 +59,8 @@ def data_abuseipdb():
     # --- check and get attacker ip ---
     ip = num_of_attack_ip().split("\n")
     # --- check and get attacker ip ---
-    for i in ip:
+    print(iconOK + ' Checking Abuseipdb')
+    for i in tqdm(ip):
         final_url = request_url.replace("IP", i)
         #print(final_url)
         info_json = requests.get(final_url)
@@ -63,12 +70,7 @@ def data_abuseipdb():
         #print(bestresult)
         #print(category_abuseipdb.retruncategory(bestresult['category']))
         #return(bestresult['ip'], category_abuseipdb.retruncategory(bestresult['category']), bestresult['country'], bestresult['abuseConfidenceScore'])
-
-        print("""\t\tIP attacker: {}
-            \tCategory: {}
-            \tCountry: {}
-            \tAbuseConfidenceScore: {}%""".format(bestresult['ip'], category_abuseipdb.retruncategory(bestresult['category']),bestresult['country'],bestresult['abuseConfidenceScore']))
-
+        
 
 def data_urlscan():
     # --- urlscan.io data ----
@@ -79,12 +81,13 @@ def data_urlscan():
     #print(request_url)
 
     context = num_of_attack_context().split(" ")[1:]
-    for i in context:
+    print(iconOK + ' Checking URLscan')
+    for i in tqdm(context):
         requests_url = url+i
-        print(requests_url)
+        #print(requests_url)
         info_json = requests.get(requests_url)
         response = json.loads(info_json.text)
-        print(response['results'][0]['page'])
+        #print(response['results'][0]['page'])
         """
         {'country': 'AU', 'server': 'Apache/2.4.10 (Debian)', 'city': '', 'domain': 'www.trellian.com', 'ip': '103.224.182.21', 'asnname': 'TRELLIAN-AS-AP Trellian Pty. Limited, AU', 'asn': 'AS133618', 'url': 'https://www.trellian.com/dsn/index.html', 'ptr': 'www.trellian.com'}
         """
@@ -98,21 +101,23 @@ def data_urlhaus():
     url = (data['API info']['urlhaus']['url'])
     # --- Attack Context ---
     context = num_of_attack_context().split(" ")[1:]
+    print(iconOK + ' Checking URLhaus')
     for i in context:
-        print(i)
+        #print(i)
         r = requests.post(url, data={"url": "http://sskymedia.com/VMYB-ht_JAQo-gi/INV/99401FORPO/20673114777/US/Outstanding-Invoices/"})
         r.raise_for_status()
-        print(r.text)
+        #print(r.text)
     
     # --- check and get attacker ip ---
     ip = num_of_attack_ip().split("\n")
+    context = num_of_attack_context().split(" ")[1:]
     # --- check and get attacker ip ---
         
-    for i in ip:
-        print(i)
+    for i in tqdm(ip):
+        #print(i)
         r = requests.post(url, data={"url": i})
         r.raise_for_status()
-        print(r.text)
+        #print(r.text)
 
 
 def data_virustotal():
@@ -126,9 +131,26 @@ def data_virustotal():
     comments_url = (data['API info']['virustotal']['comments_url'])
 
     #https://developers.virustotal.com/v2.0/reference#comments-get
+    # --- check and get attacker ip ---
+    ip = num_of_attack_ip().split("\n")
+    context = num_of_attack_context().split(" ")[1:]
+    # --- check and get attacker ip ---
+    print(iconOK + ' Checking VirusTotal')
+
+    for i in tqdm(ip):
+        params = {'apikey': api, 'resource': i}
+        response = requests.get(ip_address_url, params=params)
+        #print(response.json())
+        """
+    for x in context:
+        params = {'apikey': api, 'resource': x}
+        response = requests.get(scan_url, params=params)
+        print(response.json())
+        """
 
 
+data_urlscan()
+data_urlhaus()
+data_virustotal()
 data_abuseipdb()
-#data_urlscan()
-#data_urlhaus()
-#data_virustotal()
+
