a23e5f19b39559ad38361a3a98dd39340e8dbe50
Daniele Perera <daniele.perera@cybaze.it>
2019-03-29 14:23:11 +0100

    added func to check if webscapper is present or not

--- src/__main__.py
+++ src/__main__.py
@@ -3,11 +3,40 @@ import os
 import tempfile
 import pyperclip
 from win10toast import ToastNotifier
-#import webscapper
+from __init__ import api
+import socket
+import re
+
+
+def check_webscapper():
+    Webscapperpath = os.path.join(api, "webscapper.py")
+    exists = os.path.isfile(Webscapperpath)
+    if exists:
+        print(Webscapperpath)
+        import webscapper
+        main(webscapper.get_info())
+    else:
+        print("Il tool automatizzato per cercare gli ip malevoli non esiste, inserisci manualmente gli ip")
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--ip", dest="ip", help="somehelp bla bla", default="")
+        addr = parser.parse_args()
+        # check ip 
+        ok = check_ip(str(addr))
+
+        #print(str(ok))
+        try:
+            socket.inet_aton(str(ok))
+            attackers = {}
+            attackers['attackers'] = addr.ip.replace(',', '\n')
+            print(attackers)
+            main(attackers)
+        except socket.error:
+            print('Wrong ip address')
 
 
 def main():
-    info = {'attackers': '178.128.78.235', 'victims': 'SOCUsers', 'context': 'dns http://www.abcdefg.mn'}
+    #info = {'attackers': '178.128.78.235\n', 'victims': 'SOCUsers', 'context': 'dns http://www.abcdefg.mn'}
     # ------- Get info about attacker, victim, context from the webscapper -----
     #info = webscapper.get_info()
     
@@ -101,5 +130,12 @@ def text_body(body):
         pass
 
 
+def check_ip(ipv4_address):
+    matches = ipv4_address.split("'")[1]
+    regex_ipv4_public = r"^([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?<!172\.(16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31))(?<!127)(?<!^10)(?<!^0)\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?<!192\.168)(?<!172\.(16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31))\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(?<!\.255$)$" 
+    matches_public = re.finditer(regex_ipv4_public, matches, re.MULTILINE)
+    for i in matches_public:
+        return ("{match}".format(match=i.group()))
+
 if __name__ == '__main__':
-    main()
+    check_webscapper()
