d2a41c6dbd0312f5b57b79a5dd2bf89141e0b59d
daniele <daniele.storage1@gmail.com>
2019-04-13 20:17:04 +0200

    deleted some unused files

--- src/parser.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-class Foo:
-    def __init__(self):
-        self.parser = ArgumentParser()
-        self.parser.add_argument('-f', '--foo', default=False, action='store_true', help='foo or not?')
-        self.parser.add_argument('-b', '--bar', default=0, action='store', help='set the bar')
-        self.parser.parse_args(namespace=self)
