5772cf89f84d4bebc110060720961528c6c612ca
Daniele Perera <daniele.perera@cybaze.it>
2019-05-22 18:03:53 +0200

    working

--- src/__main__.py
+++ src/__main__.py
@@ -210,21 +210,6 @@ def collector(info: dict, verbosity_check: bool, sha_sum_list: list = None):
 if __name__ == '__main__':
     try:
         main()
-        #172.217.16.142
-        #124.164.251.179 gives ++ on virustotal
-        #178.128.78.235
-        #167.99.81.228 this one is cool
-        #188.40.75.132 this one is cool gives no data , no info , no info on abseipdb, urlhause, urlscan
-        #51.75.143.169 gives ++ on virustotal no info, no info urlscan urlhause
-        #91.80.37.231 gives ++ on virustotal no info, no info urlscan urlhause
-        #93.157.63.185 ticket completo con tutte le informazioni
-        #104.239.98.134 gives ++  Apility checking reputation and activity through time 104.239.98.134
-        #python .\src -m --attacker 124.164.251.179 --victim 124.164.251.179 works fine
-        #come vittima usa http://195.35.99.78/ è un ip che appartiene a findomestic
-        #fe-marnestg.newsletter.ecredit.it
-        """
-        aW1wb3J0IHJlcXVlc3RzCmltcG9ydCBqc29uCmltcG9ydCByZQoKYXBpID0gIkdjbHpseEptWUFLWDROSTZ5bUFUb28zRW9qVHhoaTI5Igp1cmwgPSAiaHR0cHM6Ly9hcGkuc2hvZGFuLmlvL3Nob2Rhbi9ob3N0L3t9P2tleT17fSIKaXAgPSAnMTk1LjM1Ljk5Ljc4JwpnZXRfdXJsID0gdXJsLmZvcm1hdChpcCwgYXBpKQpwcmludChnZXRfdXJsKQoKcmVzcG9uc2UgPSByZXF1ZXN0cy5nZXQoZ2V0X3VybCkKamRhdGEgPSByZXNwb25zZS5qc29uKCkKI3ByaW50KGpkYXRhKQpsaXN0X3VybCA9IFtdCmxpc3RfcG9ydHMgPSBbXQpjb3VudF9kYXRhID0gbGVuKGpkYXRhWydkYXRhJ10pCmNvdW50X3BvcnRzID0gbGVuKGpkYXRhWydwb3J0cyddKQpwcmludCgnY291bnQgcG9ydHMgaW4gamRhdGEnLCBjb3VudF9wb3J0cykKZm9yIGNvdW50IGluIHJhbmdlKGNvdW50X3BvcnRzKToKICAgIGxpc3RfcG9ydHMuYXBwZW5kKGpkYXRhWydwb3J0cyddW2NvdW50XSkKICAgICNwcmludChsaXN0X3BvcnRzKQpwcmludChsaXN0X3BvcnRzKQoKaWYgODAgaW4gbGlzdF9wb3J0czoKICAgIHVybCA9ICdodHRwOi8vJyArIGlwICsgJy8nCiAgICBwcmludCh1cmwpCiAgICBoZWFkZXIgPSB7CiAgICAgICAgIkNvbm5lY3Rpb24iOiAia2VlcC1hbGl2ZSIsCiAgICAgICAgIlVzZXItQWdlbnQiOiAiTW96aWxsYS81LjAgKE1hY2ludG9zaDsgSW50ZWwgTWFjIE9TIFggMTBfMTNfNikgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzY4LjAuMzQ0MC4xMDYgU2FmYXJpLzUzNy4zNiJ9CiAgICByZXNwb25zZSA9IHJlcXVlc3RzLmdldCh1cmwsIGhlYWRlcnM9aGVhZGVyKQogICAgdmVyYnMgPSByZXF1ZXN0cy5vcHRpb25zKHVybCkKICAgIHByaW50KHJlc3BvbnNlLmhlYWRlcnMpCiAgICBwcmludCh2ZXJicy5oZWFkZXJzWydhbGxvdyddKQppZiA0NDMgaW4gbGlzdF9wb3J0czoKICAgIHByaW50KCJodHRwcyIpCiAgICBmb3IgaSBpbiByYW5nZShjb3VudF9kYXRhKToKICAgICAgICB0cnk6CiAgICAgICAgICAgIHVybF9wYXR0ZXJuID0gamRhdGFbJ2RhdGEnXVtpXVsnc3NsJ11bJ2NlcnQnXVsnc3ViamVjdCddWydDTiddCiAgICAgICAgICAgIHRlc3QgPSB1cmxfcGF0dGVybi5yZXBsYWNlKCcqJywgJycpLnJlcGxhY2UoJy4nLCAnJywgMSkKICAgICAgICAgICAgbGlzdF91cmwuYXBwZW5kKHRlc3QpCiAgICAgICAgZXhjZXB0IEtleUVycm9yOgogICAgICAgICAgICBwYXNzCiAgICAjcHJpbnQobGlzdF91cmwpCiAgICBsaXN0X3VybCA9IGxpc3QoZGljdC5mcm9ta2V5cyhsaXN0X3VybCkpCiAgICBwcmludChsaXN0X3VybCkKICAgICNwcmludCh0eXBlKGpkYXRhKSkKICAgIGZvciB1cmwgaW4gbGlzdF91cmw6CiAgICAgICAgdHJ5OgogICAgICAgICAgICBwcmludCh1cmwpCiAgICAgICAgICAgIHJlX3BhdHRlcm4gPSByZS5jb21waWxlKHVybCkKICAgICAgICAgICAgc3RyaW5nID0ganNvbi5kdW1wcyhqZGF0YSkKICAgICAgICAgICAgbWF0Y2hlcyA9IHJlX3BhdHRlcm4uc2VhcmNoKHN0cmluZykKICAgICAgICAgICAgcHJpbnQobWF0Y2hlcy5ncm91cCgwKSkKICAgICAgICBleGNlcHQgcmUuZXJyb3I6CiAgICAgICAgICAgIHBhc3MgICAgICAgIA==
-        """
     except KeyboardInterrupt:
         print('deleting tmp files')
         print(iconOK, end='')
